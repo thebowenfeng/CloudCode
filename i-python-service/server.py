9faef10ae122b08a4b0d670aeba68fb30cbc1d99
thebowenfeng <857514.leofeng@gmail.com>
2023-07-04 13:03:47 +1200

    updated ipython service to shutdown after interactive shell close

--- i-python-service/server.py
+++ i-python-service/server.py
@@ -12,48 +12,34 @@ PORT = int(sys.argv[1])
 print(f"server started. Running on port: {PORT}")
 
 
-def is_socket_closed(sock: socket.socket) -> bool:
-    try:
-        # this will try to read bytes without blocking and also without removing them from buffer (peek only)
-        data = sock.recv(16, socket.MSG_DONTWAIT | socket.MSG_PEEK)
-        if len(data) == 0:
-            return True
-    except BlockingIOError:
-        return False  # socket is open and reading from it would block
-    except ConnectionResetError:
-        return True  # socket was closed for some other reason
-    except Exception as e:
-        print("unexpected exception when checking if a socket is closed")
-        return False
-    return False
-
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    while True:
-        conn, addr = s.accept()
-        print(f"{addr} connected")
+    conn, addr = s.accept()
+    print(f"{addr} connected")
 
-        ps = pexpect.spawn("python3")
+    ps = pexpect.spawn("python3")
 
-        def read_child_stdout():
-            global ps
-            while True:
-                try:
-                    conn.sendall(os.read(ps.child_fd, 1024))
-                except:
-                    ps = pexpect.spawn("python3")
-
-
-        child_thread = threading.Thread(target=read_child_stdout)
-        child_thread.start()
-
+    def read_child_stdout():
         while True:
-            if is_socket_closed(conn):
-                ps.terminate(force=True)
-                break
+            try:
+                conn.sendall(os.read(ps.child_fd, 1024))
+            except:
+                s.shutdown(socket.SHUT_RDWR)
+                s.close()
+                os._exit(1)
+
+
+    child_thread = threading.Thread(target=read_child_stdout)
+    child_thread.start()
+
+    while True:
+        try:
             ps.sendline(conn.recv(1024))
             ps.stdin.flush()
+        except:
+            s.shutdown(socket.SHUT_RDWR)
+            s.close()
+            break
 
 
